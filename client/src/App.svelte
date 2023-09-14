896adfbf5b8b2ab2af382f012732085239dc5c0a
edikiuspy <edikmuntyanovphone@gmail.com>
2023-09-14 10:02:33 +0200

    dziala chyba

--- client/src/App.svelte
+++ client/src/App.svelte
@@ -1,34 +1,67 @@
 <script>
-  let rand = -1;
+  let color;
+  let count = 0;
+  let rand;
+
+
 
   async function getRand() {
-    const response = await fetch("./rand");
+    const response = await fetch("./card");
     rand = await response.text();
+    await getColor();
+    await getCount();
   }
+
+  async function getCount() {
+    const response = await fetch("./count");
+    count = await response.text();
+  }
+    async function getColor() {
+    const response = await fetch("./color");
+    color = await response.text();
+    if (color === "green") {
+      color = "--MEGA, linear-gradient(180deg, #57C630 0%, #1C6B00 100%)";
+    } else if (color === "yellow") {
+      color = "--Linear, linear-gradient(180deg, #E0AB44 0%, #B76709 100%)";
+    } else if (color === "red") {
+      color = "--red, linear-gradient(180deg, #D35151 0%, #9A0000 100%)";
+    } else if (color === "black") {
+      color = "linear-gradient(180deg, #000 0%, #1D1D1D 100%);";
+    }
+        console.log(color)
+  }
+  import { onMount } from "svelte";
+
+  onMount(getRand);
 </script>
 
 <style>
-    
+  .body {
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+
   .container {
     display: flex;
+    word-wrap: break-word;
     width: 200px;
-    height: 425px;
+    height: 500px;
     padding: 76px 78px 75px 77px;
     flex-direction: column;
     justify-content: center;
     align-items: center;
-      border: 37px solid #FFF;
-    background: var(--MEGA, linear-gradient(180deg, #57C630 0%, #1C6B00 100%));
-
+    border: 37px solid #FFF;
   }
 
   h1 {
-      -webkit-text-stroke: 3px #000000;
-      text-shadow: #000000;
-      color: #FFF;
+    -webkit-text-stroke: 3px #000000;
+    text-shadow: #000000;
+    color: #FFF;
     text-align: center;
-    font-family: Inter;
-    font-size: 64px;
+    font-family: Inter, serif;
+    font-size: 50px;
     font-style: italic;
     font-weight: 900;
     line-height: normal;
@@ -47,7 +80,10 @@
   }
 </style>
 
-<div class="container">
-  <h1>Your number is {rand}!</h1>
-  <button on:click={getRand}>Get a random number</button>
+<div class="body">
+  <div class="container" style="background: var({color})">
+    <h1>{rand}</h1>
+  </div>
+  <h2>{count}</h2>
+  <button on:click={getRand}>Get card</button>
 </div>
--- server.py
+++ server.py
@@ -1,6 +1,19 @@
-from flask import Flask, send_from_directory
+from flask import Flask, send_from_directory, session, request
+from flask_session import Session  # Import Flask-Session
 import random
+import sqlite3
+
 app = Flask(__name__)
+app.config['SECRET_KEY'] = 'your_secret_key_here'  # Change this to a secure secret key
+app.config['SESSION_TYPE'] = 'filesystem'  # Store session data in the filesystem
+
+# Initialize Flask-Session
+Session(app)
+
+colors = ['green', 'yellow', 'red', 'black']
+weight = [50, 30, 10, 5]
+
+
 
 # Path for our main Svelte page
 @app.route("/")
@@ -12,11 +25,41 @@ def base():
 def home(path):
     return send_from_directory('client/public', path)
 
+@app.route("/card")
+def card():
+        global session
+        if 'x' not in session:
+            session['x'] = 0
+        session['x'] = session.get('x', 0) + 1
+        session['color'] = random.choices(colors, weights=weight, k=1)[0]
+        if 'history_images' not in session:
+            session['history_images'] = []
+        con = sqlite3.connect("Game.db")
+        cur = con.cursor()
+        images = cur.execute(f"SELECT * from Cards where Color='{session['color']}'").fetchall()
+        new_image = random.choice(images)
 
-@app.route("/rand")
-def hello():
-    return str(random.randint(0, 100))
+        while new_image in session['history_images']:
+            new_image = random.choice(images)
+            if all([new_image in session['history_images'] for new_image in images]):
+                index = colors.index(session['color'])
+                colors.remove(session['color'])
+                weight.pop(index)
+                session['color'] = random.choices(colors, weights=weight, k=1)[0]
+                images = cur.execute(f"SELECT * from Cards where Color='{session['color']}'").fetchall()
+                new_image = random.choice(images)
+        session['history_images'].append(new_image)
+        return new_image[2]
 
+
+@app.route('/color')
+def colorget():
+    global session
+    return session['color']
+
+@app.route('/count')
+def count():
+    return str(session['x'])
 
 if __name__ == "__main__":
     app.run(debug=True)
