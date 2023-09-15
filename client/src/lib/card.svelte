6c637dfa0a3303c853954a0b27031320eba66e78
re-oh <dabrowskilucas2@gmail.com>
2023-09-15 22:59:10 +0200

    started on card and textRendering

---
+++ client/src/lib/card.svelte
@@ -0,0 +1,34 @@
+<script>
+  export let color;
+  export let choiseTables;
+  export let mainText;
+
+  function fetchRenderedText() {
+    // todo
+    // fetch renderedTexted from server
+    return 
+  }
+
+
+
+</script>
+
+<main>
+
+
+  <div>
+
+      <span>
+
+        {renderText()}
+
+      </span>
+  
+  </div>
+
+
+</main>
+
+<style> 
+
+</style>
